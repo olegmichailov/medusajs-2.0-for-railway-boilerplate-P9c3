c23f8e72f58a4dffd6980328369151083f2a9db9
olegmichailov <olegmikhailov698@gmail.com>
2025-03-12 13:13:10 +0100

    Update payment-stripe.ts

--- backend/src/subscribers/payment-stripe.ts
+++ backend/src/subscribers/payment-stripe.ts
@@ -1,1 +1,21 @@
+import { Modules } from '@medusajs/framework/utils'
+import { IPaymentModuleService } from '@medusajs/framework/types'
+import { SubscriberArgs, SubscriberConfig } from '@medusajs/medusa'
 
+export default async function stripeWebhookHandler({
+  event: { data },
+  container,
+}: SubscriberArgs<any>) {
+  const paymentModuleService: IPaymentModuleService = container.resolve(Modules.PAYMENT)
+
+  try {
+    await paymentModuleService.handleWebhook("stripe", data)
+    console.log("✅ Stripe webhook processed successfully")
+  } catch (error) {
+    console.error("❌ Error processing Stripe webhook:", error)
+  }
+}
+
+export const config: SubscriberConfig = {
+  event: 'payment.stripe'
+}
