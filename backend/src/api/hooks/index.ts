709acad7dc232ad9ef6a5b0bb62bccb1e3a9e974
olegmichailov <olegmikhailov698@gmail.com>
2025-03-13 10:01:07 +0100

    Delete backend/src/api/hooks directory

--- backend/src/api/hooks/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import stripeWebhookHandler from "../webhooks/stripe"
-
-const router = Router()
-
-router.post("/hooks/payments/stripe", stripeWebhookHandler)
-
-export default router
