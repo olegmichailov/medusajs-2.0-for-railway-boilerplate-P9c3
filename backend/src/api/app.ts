6a44c00038ff08396ed238d5318cbc945e3e25a4
olegmichailov <olegmikhailov698@gmail.com>
2025-03-13 11:36:49 +0100

    Update app.ts

--- backend/src/api/app.ts
+++ backend/src/api/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import routes from "./routes"; // ✅ Этот импорт должен быть
 
 const app = express();
-app.use(express.json()); // ✅ Убедись, что `express.json()` включён
-app.use(routes); // ✅ Убедись, что маршруты подключены
+app.use(express.json()); // ✅ Без этого Stripe Webhook НЕ БУДЕТ работать
+app.use(routes);
 
 export default app;
