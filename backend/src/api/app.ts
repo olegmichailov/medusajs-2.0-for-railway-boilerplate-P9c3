a3cd0a28289bd4892d5c59fdbcfe9db50951a270
olegmichailov <olegmikhailov698@gmail.com>
2025-03-14 10:36:36 +0100

    Delete backend/src/api/app.ts

--- backend/src/api/app.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import routes from "./routes"; // ✅ Этот импорт должен быть
-
-const app = express();
-app.use(express.json()); // ✅ Без этого Stripe Webhook НЕ БУДЕТ работать
-app.use(routes);
-
-export default app;
