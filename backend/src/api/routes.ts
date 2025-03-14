c608503c58125be91d574e68d367f699209e5675
olegmichailov <olegmikhailov698@gmail.com>
2025-03-14 10:36:24 +0100

    Delete backend/src/api/routes.ts

--- backend/src/api/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import stripeWebhookHandler from "./hooks/payments/stripe";
-
-const router = Router();
-
-// Stripe Webhook
-router.use("/hooks/payments/stripe", stripeWebhookHandler);
-
-export default router;
